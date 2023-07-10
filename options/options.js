1fc81a32c40e7c3d53893d0c0a29918a1caa81bd
Crowdbotics <team@crowdbotics.com>
2023-07-10 23:23:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broad-recipe-42079",
+  url: "https://broad-recipe-42079.botics.co",
+  api: "https://broad-recipe-42079.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
